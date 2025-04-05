879cd5e447d5cff2ffc96f47a09e446d2166b649
Henry <hmallen99@gmail.com>
2025-04-05 14:28:40 -0400

    Initialize WebGPU device

--- libs/chow-engine/src/lib/engine/Engine.ts
+++ libs/chow-engine/src/lib/engine/Engine.ts
@@ -1,4 +1,4 @@
-import { Scene } from './scene.js';
+import { Scene } from './Scene.js';
 
 /**
  * The Basic WebGPU Engine.
@@ -7,10 +7,55 @@ import { Scene } from './scene.js';
  */
 export class Engine {
   private scenes: Scene[] = [];
+  private _canvas: HTMLCanvasElement;
+  private _context: GPUCanvasContext;
+  private _ready = false;
+  private _adapter: GPUAdapter | null = null;
+  private _device: GPUDevice | null = null;
+
+  constructor(canvas: HTMLCanvasElement) {
+    this._canvas = canvas;
+    const context = canvas.getContext('webgpu');
+    if (!context) throw new Error('Failed to intialize WebGPU context');
+    this._context = context;
+
+    navigator.gpu
+      ?.requestAdapter({
+        featureLevel: 'compatibility',
+      })
+      .then((adapter) => {
+        this._adapter = adapter;
+        return adapter?.requestDevice();
+      })
+      .then((device) => {
+        this._device = device ?? null;
+        this._ready = !!this._device && !!this._adapter;
+      });
+  }
 
   public createScene() {
     const nextScene = new Scene(this);
     this.scenes.push(nextScene);
     return nextScene;
+  }
+
+  public get adapter() {
+    return this._adapter;
+  }
+
+  public get device() {
+    return this._device;
+  }
+
+  public get ready() {
+    return this._ready;
+  }
+
+  public get canvas() {
+    return this._canvas;
+  }
+
+  public get context() {
+    return this._context;
   }
 }
